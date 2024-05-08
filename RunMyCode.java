a297065d56aecb0f01b9f1ff5919a017343b534f
Limex7 <126993571+Limex7@users.noreply.github.com>
2024-05-07 17:07:18 -0700

    Update RunMyCode.java
    made it the calls to phonebook and added some shows of my methods

--- RunMyCode.java
+++ RunMyCode.java
@@ -1,12 +1,11 @@
 public class RunMyCode {
     public static void main(String[] args){
-        ListNode cool = new ListNode(10);
+        PhoneBook cool = new PhoneBook("Conner", "Wilson", "123 ahhhhh", "burlington", "001001001");
         Builder build = new Builder();
         for (int i = 0; i < 3; i++){
-            build.add(cool, i * 10);
+            build.add(cool, "Jon" + i, "Jonzz", "1912 st", "arlington", "s001001002");
         }
-        build.add(cool, 100, 1);
-        build.remove(cool, 3);
+        build.set(cool, "les", "go", "1912 st", "bellingham", "1111111111", 2);
         build.print(cool);
     }
 }
