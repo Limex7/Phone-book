2f0fd3b8ebf456152230b2751461856698647b35
Limex7 <126993571+Limex7@users.noreply.github.com>
2024-05-07 17:05:59 -0700

    Update Builder.java
    made it all phonebook instead of listNode

--- Builder.java
+++ Builder.java
@@ -1,14 +1,14 @@
 public class Builder {
-    public void print(ListNode start){
-        ListNode current = start;
+    public void print(PhoneBook start){
+        PhoneBook current = start;
         while (current.next != null){
             System.out.println(current.toString());
             current = current.next;
         }
         System.out.println(current.toString());
     }
-    public void print(ListNode start, int element){
-        ListNode current = start;
+    public void print(PhoneBook start, int element){
+        PhoneBook current = start;
         int i = 0;
         int out = 0;
         while (current.next != null){
@@ -24,20 +24,20 @@ public class Builder {
             System.out.println(current.toString());
         }
     }
-    public void add(ListNode start, String fName, String lName, String adress, String city, String phoneNumber){
-        ListNode send = start;
+    public void add(PhoneBook start, String fName, String lName, String adress, String city, String phoneNumber){
+        PhoneBook send = start;
         while (send.next != null){
             send = send.next;
         }
-        send.next = new ListNode(fName, lName, adress, city, phoneNumber);
+        send.next = new PhoneBook(fName, lName, adress, city, phoneNumber);
     }
-    public void add(ListNode start, String fName, String lName, String adress, String city, String phoneNumber, int location){ //extension of the add method (makes location a optional parameter)
-        ListNode current = start;
+    public void add(PhoneBook start, String fName, String lName, String adress, String city, String phoneNumber, int location){ //extension of the add method (makes location a optional parameter)
+        PhoneBook current = start;
         int i = 0;
         int out = 0;
         while (current.next != null){
             if (i == location){
-                current.next = new ListNode("", fName, lName, adress, city, phoneNumber, current.next);
+                current.next = new PhoneBook("", fName, lName, adress, city, phoneNumber, current.next);
                 out++;
                 break;
             }
@@ -48,11 +48,11 @@ public class Builder {
             current = current.next;
         }
         if (out != 1){
-            current.next =  new ListNode(fName, lName, adress, city, phoneNumber, current.next);
+            current.next =  new PhoneBook(fName, lName, adress, city, phoneNumber, current.next);
         }
     }
-    public void remove(ListNode start, int location){
-        ListNode current = start;
+    public void remove(PhoneBook start, int location){
+        PhoneBook current = start;
         int i = 0;
         int out = 0;
         while (current.next != null){
@@ -71,8 +71,8 @@ public class Builder {
             current.next = null;
         }
     }
-    public void set(ListNode start, String fName, String lName, String adress, String city, String phoneNumber, int location){
-        ListNode current = start;
+    public void set(PhoneBook start, String fName, String lName, String adress, String city, String phoneNumber, int location){
+        PhoneBook current = start;
         int i = 0;
         int out = 0;
         while (current.next != null){
