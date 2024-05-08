025ec81e638c8ab6e0c378ec6e468028d0158dca
Limex7 <126993571+Limex7@users.noreply.github.com>
2024-05-07 17:24:33 -0700

    Update RunMyCode.java
    now it runs all methods

--- RunMyCode.java
+++ RunMyCode.java
@@ -5,7 +5,12 @@ public class RunMyCode {
         for (int i = 0; i < 3; i++){
             build.add(cool, "Jon" + i, "Jonzz", "1912 st", "arlington", "s001001002");
         }
+        build.print(cool, 2);
         build.set(cool, "les", "go", "1912 st", "bellingham", "1111111111", 2);
+        build.print(cool, 2);
+        build.remove(cool, 2);
+        build.print(cool, 2);
+        build.add(cool, "person", "person", "11111 person Lane", "Persington", "737766777");
         build.print(cool);
     }
 }
